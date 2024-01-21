8f5490cefbb633bef1505dde3f42ce1728f1a525
Andrey Zotov <mail@mail.mail>
2024-01-21 21:01:08 +0000

    add hw1

--- task1/server.py
+++ task1/server.py
@@ -1,16 +1,14 @@
 
 from collections import Counter
 from flask import Flask
-from flask import jsonify
-from flask import request
-from flask import abort
+from flask import jsonify, request, abort
 
 def read_dictionary(filename: str) -> Counter:
     '''
         Read dictionary file with words statistics.
         Function results is Counter datatype.
     '''
-    #YOUR CODE HERE
+    #YOUR CODE HERE FROM PREVIOUS CELL
     words = dict()
     with open(filename, 'r') as f:
         for line in f:
@@ -20,33 +18,6 @@ def read_dictionary(filename: str) -> Counter:
     return Counter(words)
 
 WORDS = read_dictionary('dictionary.txt')
-
-
-def P(word, N=sum(WORDS.values())): 
-    '''
-        Probability of `word`: (num occurances of `word`)/ (total count of words) 
-    '''
-    # YOUR CODE HERE
-    return WORDS[word]/N
-
-def most_probable(word): 
-    '''
-        Find most probable (with max ) spelling correction for word. 
-        Hint: see max function + key param 
-            https://www.programiz.com/python-programming/methods/built-in/max
-    '''
-    # YOUR CODE HERE
-    return max(candidates(word), key=lambda w: P(w))
-
-def candidates(word): 
-    '''
-        Generate most nearest spelling corrections for word.
-        If found word in dictionary then return word, otherwise
-        try found words from one and then two edit distance
-    '''
-    one_symbol_change = generate_candidates_one_symbol(word)
-    two_symbol_change = generate_candidates_two_symbol(word)
-    return (known([word]) or known(one_symbol_change) or known(two_symbol_change) or [word])
 
 def known(words): 
     '''
@@ -78,28 +49,8 @@ def generate_candidates_two_symbol(word):
 
 app = Flask(__name__)
 
-@app.route('/version', methods=['GET'])
-def get_version():
-    # YOUR CODE HERE
-    return jsonify({'version':'1.0'})
-
-@app.route('/correct', methods=['GET'])
-def correct():
-    # YOUR CODE HERE
-    check_word = request.args.get('check_word')
-    corrected = most_probable(check_word)
-    return jsonify({'correct_word': corrected})
-
-@app.route('/add_word', methods=['POST'])
-def add_word():
-    # YOUR CODE HERE
-    added_word =  request.json['added_word']
-    WORDS[added_word] += 1
-
-    return jsonify(success=True)
-
 @app.route('/candidates/<int:edit_distance>', methods=['GET'])
-def get_candidates(edit_distance: int):
+def candidates(edit_distance: int):
     # YOUR CODE HERE
     word =  request.args.get('word')
     one_symbol_change = generate_candidates_one_symbol(word)
@@ -110,6 +61,8 @@ def get_candidates(edit_distance: int):
     }
     
     return jsonify(responses[edit_distance]) if edit_distance in responses else abort(404)
+
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
